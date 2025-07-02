924f3d0dbace7b4102a817766c8c79627af287a9
mm04-3 <mk@yantoswire.com>
2025-07-02 16:50:05 +0300

    Add files via upload

---
+++ addons/yantos_invoice_template/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Yantos Invoice Template",
+    "version": "1.0",
+    "author": "YANTOS",
+    "category": "Accounting",
+    "depends": ["account"],
+    "data": ["views/report_invoice_document.xml"],
+    "installable": True,
+    "application": False
+}
